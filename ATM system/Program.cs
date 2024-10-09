8f546248cc0eb72d9c2fcc77c820083330eba0cd
Archie Barnett <a.barnett@student.reading.ac.uk>
2024-10-09 16:53:58 +0100

    Ability to read file

--- ATM system/Program.cs
+++ ATM system/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System.IO;
+internal class Program
 {
     private static void Main(string[] args)
     {
+        // Read dummy file of PINs for comparison
+        var path = "C:\\Users\\archb\\OneDrive\\Documents\\C# Projects\\ATM system\\PINData.txt";
+
         Console.WriteLine("Welcome, please enter PIN to get started! Or write PIN to set up a new PIN");
 
         // Input might be null, check for null values
@@ -19,5 +23,9 @@
                 Console.WriteLine("Nothing was set");
             }
         }
+
+        // Reading the PIN values from the file of absolute path
+        StreamReader pinFile = new(path);
+        string pinsToString = pinFile.ReadToEnd();
     }
 }
--- ATM system/obj/Debug/net8.0/ATM system.AssemblyInfo.cs
+++ ATM system/obj/Debug/net8.0/ATM system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2490a1328d56533be5d083a2b6677274fcca637f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0fd562b9faf38acd411db0ebdd973960c50d8ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
